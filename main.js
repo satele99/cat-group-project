69de89f1dd530406c28304348362ac62b68923f4
Amir Ali <satele.works@gmail.com>
2022-12-14 11:28:50 -0500

    working on filter func in js

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 let catNames = document.getElementById('cat-names');
 let catLink = "https://api.thecatapi.com/v1/breeds"; 
+let getNamesDone = false;
 
 function callApi() {
    axios.get('https://api.thecatapi.com/v1/breeds').then(response => {
-        for(i = 0; i < response.data.length; i++){
-            let newDiv = document.createElement('div');
-            catNames.appendChild(newDiv);
-            newDiv.innerText = response.data[i].name;
-        };
+        getNames(response.data);
    })
+} 
+
+function getNames(object) {
+    catNames = document.getElementById('cat-names');
+    if(getNamesDone == false){
+        for(i = 0; i < object.length; i++){
+            let catBreed = document.createElement('div');
+            let hyperLink = document.createElement('a');
+    
+            catNames.appendChild(catBreed);
+            catBreed.className = 'cat-names';
+            catBreed.appendChild(hyperLink);
+            hyperLink.innerText = object[i].name;
+            hyperLink.setAttribute('onclick', 'getDescription()');
+            hyperLink.className = 'cat-names';
+        }
+        getNamesDone = true;
+    }
 }
