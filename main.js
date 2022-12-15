907b0ffc237c85eb712f23066456c1f0b7630fcc
Amir Ali <satele.works@gmail.com>
2022-12-15 16:38:01 -0500

    finished filter functionality

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let catNames = document.getElementById('cat-names');
 let catLink = "https://api.thecatapi.com/v1/breeds"; 
 let getNamesDone = false;
 let getDescDone = false; 
-console.log('amir branch');
+
 
 function callApi() {
    axios.get(catLink).then(response => {
@@ -20,36 +20,53 @@ function getDescription(id, object) {
         console.log(object[id].name);
         let catDescription = document.getElementById('cat-description');
         let des = document.createElement('div');
+        let imgButton = document.createElement('button');
         catDescription.appendChild(des);
         des.innerText = object[id].description;
+        des.appendChild(imgButton);
+        imgButton.innerText = "See Image";
+        imgButton.setAttribute('onclick', 'getImage()');
         getDescDone = true;
     }
 }
 function getNames(object) {
     catNames = document.getElementById('cat-names');
     let slider = document.getElementById('slider');
+    let slider2 = document.getElementById('slider2');
+    let slider3 = document.getElementById('slider3');
 
     if(getNamesDone == false){
         for(i = 0; i < object.length; i++){
             let catBreed = document.createElement('div');
             let hyperLink = document.createElement('a');
-            if (slider.value <= 0) {
-                catNames.appendChild(catBreed);
-                catBreed.className = 'cat-names';
-                catBreed.appendChild(hyperLink);
-                hyperLink.innerText = object[i].name;
-                hyperLink.id = i;
-                hyperLink.setAttribute('onclick', "getApi("+hyperLink.id+")");
-                hyperLink.className = 'cat-names';
-            } else if (slider.value == object[i].energy_level) {
-                catNames.appendChild(catBreed);
-                catBreed.className = 'cat-names';
-                catBreed.appendChild(hyperLink);
-                hyperLink.innerText = object[i].name;
-                hyperLink.id = i;
-                hyperLink.setAttribute('onclick', "getApi("+hyperLink.id+")");
-                hyperLink.className = 'cat-names';
+            let x = 0;
+
+            if ((slider.value == 0) &&
+                (slider2.value == 0) &&
+                (slider3.value == 0)) {
+                    catNames.appendChild(catBreed);
+                    catBreed.className = 'cat-names';
+                    catBreed.appendChild(hyperLink);
+                    hyperLink.innerText = object[i].name;
+                    hyperLink.id = i;
+                    hyperLink.setAttribute('onclick', "getApi("+hyperLink.id+")");
+                    hyperLink.className = 'cat-names';
+                
+            } else if (slider.value == object[i].energy_level){
+                if (slider2.value == object[i].affection_level){
+                    if(slider3.value == object[i].intelligence) {
+                        catNames.appendChild(catBreed);
+                        catBreed.className = 'cat-names';
+                        catBreed.appendChild(hyperLink);
+                        hyperLink.innerText = object[i].name;
+                        hyperLink.id = i;
+                        hyperLink.setAttribute('onclick', "getApi("+hyperLink.id+")");
+                        hyperLink.className = 'cat-names';
+                        console.log(x + " all filters set are true");
+                    }
+                }
             }
+            
         }
         getNamesDone = true;
     }
