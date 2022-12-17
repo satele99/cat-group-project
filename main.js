c7db774548b1a9fb78efadc350bf9049301c6adf
Amir Ali <satele.works@gmail.com>
2022-12-17 13:04:02 -0500

    filter updated to be more fficient

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let catNames = document.getElementById('cat-names');
 let catLink = "https://api.thecatapi.com/v1/breeds"; 
 let getNamesDone = false;
 let getDescDone = false; 
+let getImgDone = false;
 
 
 function callApi() {
@@ -17,7 +18,6 @@ function getApi(id) {
 }
 function getDescription(id, object) {
     if (getDescDone == false) {
-        // console.log(object[id].name);
         let catDescription = document.getElementById('cat-description');
         let des = document.createElement('div');
         let div2 = document.createElement('div');
@@ -33,18 +33,20 @@ function getDescription(id, object) {
         imgButton.setAttribute('onclick', "getImage("+imgButton.id+")");
         getDescDone = true;
     }
-    // else if (getDescDone == true) {
-    //     let dynDiv = document.getElementById('desc-div');
-    //     let imgDiv = document.getElementById('desc-img');
-    //     let div3 = document.createElement('div');
-    //     let imgButton2 = document.createElement('button');
-    //     dynDiv.innerText = object[id].description;
-    //     dynDiv.appendChild(imgDiv);
-    //     div3.appendChild(imgButton2);
-    //     imgButton2.innerText = "See Image";
-    //     imgButton2.id = id
-    //     imgButton2.setAttribute('onclick', "getImage("+imgButton2.id+")");
-    // }
+    else if (getDescDone == true) {
+        let dynDiv = document.getElementById('desc-div');
+        let imgDiv = document.getElementById('desc-img');
+
+        imgButton = document.createElement('button');
+        div2 = document.createElement('div');
+        dynDiv.innerText = object[id].description;
+        dynDiv.appendChild(div2);
+        div2.id = 'desc-img';
+        div2.appendChild(imgButton);
+        imgButton.innerText = "See Image";
+        imgButton.id = id;
+        imgButton.setAttribute('onclick', "getImage("+imgButton.id+")");
+    }
 }
 function getImage(num) {
     axios.get(catLink).then(response => {
@@ -52,18 +54,37 @@ function getImage(num) {
         for(i =0; i < response.data.length; i++) {
             if(num == i){
                 imgId = response.data[num].id;
+                console.log(imgId);
             }
         }
-        axios.get('https://api.thecatapi.com/v1/images/search?breed_ids='+imgId).then(response => {
-            let catImage = document.getElementById('cat-image');
-            let imgTag = document.createElement('img');
+        if (getImgDone == false) {
+            axios.get('https://api.thecatapi.com/v1/images/search?breed_ids='+imgId).then(response => {
+                let catImage = document.getElementById('cat-image');
+                let catDiv = document.createElement('div');
+                let imgTag = document.createElement('img');
 
-            catImage.appendChild(imgTag);
-            imgTag.setAttribute('src', response.data[0].url);
-            imgTag.setAttribute('width', 253);
-            imgTag.setAttribute('height', 210);
-            imgTag.className = 'image';
-        })
+                catImage.appendChild(catDiv);
+                catDiv.id = 'catDiv';
+                catDiv.appendChild(imgTag);
+                imgTag.setAttribute('src', response.data[0].url);
+                imgTag.setAttribute('width', 253);
+                imgTag.setAttribute('height', 210);
+                imgTag.className = 'image';
+                getImgDone = true;
+            })
+        }else if(getImgDone == true) {
+            axios.get('https://api.thecatapi.com/v1/images/search?breed_ids='+imgId).then(response => {
+                let catDiv = document.getElementById('catDiv');
+                let imgTag = document.createElement('img');
+
+                catDiv.innerHTML = "";
+                catDiv.appendChild(imgTag);
+                imgTag.setAttribute('src', response.data[0].url);
+                imgTag.setAttribute('width', 253);
+                imgTag.setAttribute('height', 210);
+                imgTag.className = 'image';
+            })
+        }
     })
 }
 function getNames(object) {
@@ -71,6 +92,7 @@ function getNames(object) {
     let slider = document.getElementById('slider');
     let slider2 = document.getElementById('slider2');
     let slider3 = document.getElementById('slider3');
+    let slider4 = document.getElementById('slider4');
 
     if(getNamesDone == false){
         for(i = 0; i < object.length; i++){
@@ -80,7 +102,8 @@ function getNames(object) {
 
             if ((slider.value == 0) &&
                 (slider2.value == 0) &&
-                (slider3.value == 0)) {
+                (slider3.value == 0) &&
+                (slider4.value == 0)) {
                     catNames.appendChild(catBreed);
                     catBreed.className = 'cat-names';
                     catBreed.appendChild(hyperLink);
@@ -92,6 +115,7 @@ function getNames(object) {
             } else if (slider.value == object[i].energy_level){
                 if (slider2.value == object[i].affection_level){
                     if(slider3.value == object[i].intelligence) {
+                        if(slider4.value == object[i].child_friendly)
                         catNames.appendChild(catBreed);
                         catBreed.className = 'cat-names';
                         catBreed.appendChild(hyperLink);
@@ -102,14 +126,10 @@ function getNames(object) {
                         console.log(x + " all filters set are true");
                     }
                 }
-            }else {
-                catNames.appendChild(catBreed);
-                catBreed.innerText = 'No Results Found';
-                break
-            } 
+            }
         }
         getNamesDone = true;
-    }
+    } 
 }; 
 function reset() {
     location.reload();
